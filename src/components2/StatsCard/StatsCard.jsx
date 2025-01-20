3981c1d3f1fa498da1f1ed31dc93c27c89f91d74
sainesh25 <patilsainesh25@gmail.com>
2025-01-20 23:28:54 +0530

    stats card added

---
+++ src/components2/StatsCard/StatsCard.jsx
@@ -0,0 +1,18 @@
+export function StatsCard({ title, value, lastSevenDays }) {
+    return (
+        <div className={`p-4 rounded-lg shadow-md hover:bg-[#c8e1f9] hover:shadow-xl transition-all cursor-pointer bg-white flex-grow mt-2`}>
+            <h3 className="text-gray-500 text-md font-medium">{title}</h3>
+            <p className="text-lg font-semibold text-gray-500 mt-1">{value}</p>
+            <hr className="mt-4"/>
+            {lastSevenDays && (
+                <div className="mt-4">
+                    <p className="text-xs text-gray-500">Last 7 Days</p>
+                    <p className="text-md">
+                        <span className="text-green-500">Avg: </span>
+                        <span className="text-green-500">{lastSevenDays.value}</span>
+                    </p>
+                </div>
+            )}
+        </div>
+    )
+}
